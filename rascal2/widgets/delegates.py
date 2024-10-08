df0bc7653dd3c912dcc1f25a1c035f5647c42df6
alexhroom <alex.room@btinternet.com>
2024-10-08 07:40:39 +0100

    replaced delegates with validated input delegate

--- rascal2/widgets/delegates.py
+++ rascal2/widgets/delegates.py
@@ -2,39 +2,19 @@
 
 from PyQt6 import QtCore, QtGui, QtWidgets
 
+from rascal2.widgets.inputs import ValidatedInputWidget
 
-class EnumDelegate(QtWidgets.QStyledItemDelegate):
-    """Item delegate for Enums."""
 
-    def __init__(self, parent, enum):
+class ValidatedInputDelegate(QtWidgets.QStyledItemDelegate):
+    """Item delegate for validated inputs."""
+
+    def __init__(self, parent, input):
         super().__init__(parent)
-        self.enum = enum
+        self.input = input
 
     def createEditor(self, parent, option, index):
-        combobox = QtWidgets.QComboBox(parent)
-        combobox.addItems(str(e.value) for e in self.enum)
-        return combobox
-
-    def setEditorData(self, editor: QtWidgets.QCheckBox, index):
+        return ValidatedInputWidget(parent)
+    
+    def setEditorData(self, editor: QtWidgets.QWidget, index):
         data = index.data(QtCore.Qt.ItemDataRole.DisplayRole)
-        editor.setCurrentText(data)
-
-
-class BoolDelegate(QtWidgets.QStyledItemDelegate):
-    """Item delegate for bools."""
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.parent = parent
-
-    def createEditor(self, parent, option, index):
-        checkbox = QtWidgets.QCheckBox(parent)
-        # fill in background as otherwise you can see the original View text underneath
-        checkbox.setAutoFillBackground(True)
-        checkbox.setBackgroundRole(QtGui.QPalette.ColorRole.Base)
-        return checkbox
-
-    def setEditorData(self, editor: QtWidgets.QCheckBox, index):
-        data = index.data(QtCore.Qt.ItemDataRole.DisplayRole)
-        data = data == "True"  # data from model is given as a string
-        editor.setChecked(data)
+        editor.set_data(data)
