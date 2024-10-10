7a60895d272100754e827098f4831c49cdbfb9d7
alexhroom <alex.room@btinternet.com>
2024-10-10 14:42:38 +0100

    added experimental params widget

--- rascal2/widgets/delegates.py
+++ rascal2/widgets/delegates.py
@@ -1,5 +1,5 @@
 """Delegates for items in Qt tables."""
-from enum import Enum
+
 
 from PyQt6 import QtCore, QtGui, QtWidgets
 
@@ -11,10 +11,15 @@ class ValidatedInputDelegate(QtWidgets.QStyledItemDelegate):
 
     def __init__(self, field_info, parent):
         super().__init__(parent)
+        self.table = parent
         self.field_info = field_info
 
     def createEditor(self, parent, option, index):
         widget = ValidatedInputWidget(self.field_info, parent)
+
+        # without this signal, data isn't committed unless the user presses 'enter' on the cell
+        widget.edited_signal.connect(lambda: self.table.commitData(widget))
+
         # fill in background as otherwise you can see the original View text underneath
         widget.setAutoFillBackground(True)
         widget.setBackgroundRole(QtGui.QPalette.ColorRole.Base)
@@ -28,9 +33,3 @@ class ValidatedInputDelegate(QtWidgets.QStyledItemDelegate):
     def setModelData(self, editor, model, index):
         data = editor.get_data()
         model.setData(index, data, QtCore.Qt.ItemDataRole.EditRole)
-
-    def paint(self, painter, option, index):
-        if issubclass(self.field_info.annotation, Enum):
-            label = QtWidgets.QLabel(str(index.data(QtCore.Qt.ItemDataRole.DisplayRole)))
-            return label.render(painter)
-        return super().paint(painter, option, index)
