1b188dd7c33a6c13ffeabcc8289e8a1aa5f40cc7
alexhroom <alex.room@btinternet.com>
2025-04-09 14:15:50 +0100

    rewrote plot tests to be just for sld widget

--- tests/widgets/test_plot.py
+++ tests/widgets/test_plot.py
@@ -4,7 +4,7 @@ import pytest
 import RATapi
 from PyQt6 import QtWidgets
 
-from rascal2.widgets.plot import PlotWidget
+from rascal2.widgets.plot import RefSLDWidget
 
 
 class MockWindowView(QtWidgets.QMainWindow):
@@ -20,34 +20,34 @@ view = MockWindowView()
 
 
 @pytest.fixture
-def plot_widget():
-    plot_widget = PlotWidget(view)
-    plot_widget.canvas = MagicMock()
+def sld_widget():
+    sld_widget = RefSLDWidget(view)
+    sld_widget.canvas = MagicMock()
 
-    return plot_widget
+    return sld_widget
 
 
-def test_toggle_setting(plot_widget):
+def test_ref_sld_toggle_setting(sld_widget):
     """Test that plot settings are hidden when the button is toggled."""
-    assert not plot_widget.plot_controls.isVisibleTo(plot_widget)
-    plot_widget.toggle_button.toggle()
-    assert plot_widget.plot_controls.isVisibleTo(plot_widget)
-    plot_widget.toggle_button.toggle()
-    assert not plot_widget.plot_controls.isVisibleTo(plot_widget)
+    assert not sld_widget.plot_controls.isVisibleTo(sld_widget)
+    sld_widget.toggle_button.toggle()
+    assert sld_widget.plot_controls.isVisibleTo(sld_widget)
+    sld_widget.toggle_button.toggle()
+    assert not sld_widget.plot_controls.isVisibleTo(sld_widget)
 
 
 @patch("RATapi.plotting.RATapi.plotting.plot_ref_sld_helper")
-def test_plot_event(mock_plot_sld, plot_widget):
+def test_ref_sld_plot_event(mock_plot_sld, sld_widget):
     """Test that plot helper recieved correct flags from UI ."""
     data = RATapi.events.PlotEventData()
     data.contrastNames = ["Hello"]
 
-    assert plot_widget.current_plot_data is None
-    plot_widget.plot_event(data)
-    assert plot_widget.current_plot_data is data
+    assert sld_widget.current_plot_data is None
+    sld_widget.plot_event(data)
+    assert sld_widget.current_plot_data is data
     mock_plot_sld.assert_called_with(
         data,
-        plot_widget.figure,
+        sld_widget.figure,
         delay=False,
         linear_x=False,
         q4=False,
@@ -55,12 +55,12 @@ def test_plot_event(mock_plot_sld, plot_widget):
         show_grid=False,
         show_legend=True,
     )
-    plot_widget.canvas.draw.assert_called_once()
+    sld_widget.canvas.draw.assert_called_once()
     data.contrastNames = []
-    plot_widget.plot_event(data)
+    sld_widget.plot_event(data)
     mock_plot_sld.assert_called_with(
         data,
-        plot_widget.figure,
+        sld_widget.figure,
         delay=False,
         linear_x=False,
         q4=False,
@@ -69,14 +69,14 @@ def test_plot_event(mock_plot_sld, plot_widget):
         show_legend=False,
     )
     data.contrastNames = ["Hello"]
-    plot_widget.x_axis.setCurrentText("Linear")
-    plot_widget.y_axis.setCurrentText("Q^4")
-    plot_widget.show_error_bar.setChecked(False)
-    plot_widget.show_grid.setChecked(True)
-    plot_widget.show_legend.setChecked(False)
+    sld_widget.x_axis.setCurrentText("Linear")
+    sld_widget.y_axis.setCurrentText("Q^4")
+    sld_widget.show_error_bar.setChecked(False)
+    sld_widget.show_grid.setChecked(True)
+    sld_widget.show_legend.setChecked(False)
     mock_plot_sld.assert_called_with(
         data,
-        plot_widget.figure,
+        sld_widget.figure,
         delay=False,
         linear_x=True,
         q4=True,
@@ -87,13 +87,13 @@ def test_plot_event(mock_plot_sld, plot_widget):
 
 
 @patch("RATapi.inputs.make_input")
-def test_plot(mock_inputs, plot_widget):
-    """Test that plot settings are hidden when the button is toggled."""
+def test_ref_sld_plot(mock_inputs, sld_widget):
+    """Test that the plot is made when given a plot event."""
     project = MagicMock()
     result = MagicMock()
     data = MagicMock
     with patch("RATapi.events.PlotEventData", return_value=data):
-        assert plot_widget.current_plot_data is None
-        plot_widget.plot(project, result)
-        assert plot_widget.current_plot_data is data
-        plot_widget.canvas.draw.assert_called_once()
+        assert sld_widget.current_plot_data is None
+        sld_widget.plot(project, result)
+        assert sld_widget.current_plot_data is data
+        sld_widget.canvas.draw.assert_called_once()
